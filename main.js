f51c930c5932fb80da5e900fb9c57e13e3acca72
Johan Uribe <49119190+johanuribe03@users.noreply.github.com>
2019-08-16 22:55:19 -0400

    final version, filterItems function complete

--- main.js
+++ main.js
@@ -56,5 +56,15 @@ function filterItems(e){
     var text = e.target.value.toLowerCase();
     // Get lis
     var items = itemList.getElementsByTagName('li');
-    
+    // Convert to an array
+    Array.from(items).forEach(function(item)
+        {
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+        }
+    )
 }
